46cbcd0706093d4aa5fcdfe51d62bb96cc958615
OrenMustaki <github@mustaki.com>
2018-07-14 15:56:52 +0300

    added summary at the end

--- Rotate.py
+++ Rotate.py
@@ -6,7 +6,6 @@
 # 00 00-23,3 * * * /sbin/zfs snapshot pool01@$(/bin/date "+\%Y_\%m_\%d-\%H_\%M_\%S.hourly")
 # 00 00 * * 6 /sbin/zfs snapshot pool01@$(/bin/date "+\%Y_\%m_\%d-\%H_\%M_\%S.weekly")
 #
-###
 
 import subprocess as sp
 import argparse
@@ -17,7 +16,7 @@ parser.add_argument('-q', '--quiet', help='destroy snapshots without confirmatio
 a = parser.parse_args()
 
 # number of most recent snapshots to keep
-_h = 24 * 4
+_h = 24 * 7
 _d = 30
 _w = 52
 
@@ -30,9 +29,13 @@ for line in snapshots:
     schedule = snapshot.split('.')[-1]
     snaps[schedule].append(snapshot)
 
-destroy_list = snaps['hourly'][:len(snaps['hourly']) - _h]
-destroy_list += snaps['daily'][:len(snaps['daily']) - _d]
-destroy_list += snaps['weekly'][:len(snaps['weekly']) - _w]
+h_del = snaps['hourly'][:len(snaps['hourly']) - _h]
+d_del = snaps['daily'][:len(snaps['daily']) - _d]
+w_del = snaps['weekly'][:len(snaps['weekly']) - _w]
+
+destroy_list = h_del
+destroy_list += d_del
+destroy_list += w_del
 
 for snapshot in destroy_list:
     cmd = f'zfs destroy {snapshot}'
@@ -45,5 +48,10 @@ for snapshot in destroy_list:
     print('deleting')
     print(sp.getoutput(f'{cmd}'))
 
+print(f"""
+deleted : {len(w_del)} hourly snapshots'
+          {len(d_del)} daily snapshots'
+          {len(w_del)} weekly snapshots'
+""")
 
 
